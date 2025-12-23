ae291efb10d3e16844850118c2023e55cdb3da3e
Martin Khachatryan <86410695+Martin9204@users.noreply.github.com>
2025-12-23 16:19:45 +0400

    mixedCrossLiquidityV3AgainstV4Supported leaving for base only

--- lib/handlers/injector-sor.ts
+++ lib/handlers/injector-sor.ts
@@ -536,17 +536,7 @@ export abstract class InjectorSOR<Router, QueryParams> extends Injector<
                 ChainId.MONAD
             ]
 
-          const mixedCrossLiquidityV3AgainstV4Supported = [
-                ChainId.MAINNET,
-                ChainId.ARBITRUM_ONE,
-                ChainId.BASE,
-                ChainId.POLYGON,
-                ChainId.OPTIMISM,
-                ChainId.AVALANCHE,
-                ChainId.BNB,
-                ChainId.UNICHAIN,
-                ChainId.MONAD
-            ]
+          const mixedCrossLiquidityV3AgainstV4Supported = [ChainId.BASE]
 
           const cachedRoutesCacheInvalidationFixRolloutPercentage = NEW_CACHED_ROUTES_ROLLOUT_PERCENT[chainId]
 
